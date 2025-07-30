addc806d40afe397e75ed3b6d5fe192998e484f1
malik-zulfi <malikzulfi@gmail.com>
2025-07-30 07:11:03 +0300

    AI Model Change

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-2.5-pro',
   temperature: 0.0,
 });
